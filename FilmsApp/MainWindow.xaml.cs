d4d5fcff396b6e7a11ddc30c10775a95550ac54f
Voe <mr.subiter@mail.ru>
2023-03-18 23:55:27 +0500

    Create methods "CreateTable" and "FillTable" "CreateTable" method creating table wich going to be a keeper for table from DB. "FillTable" method filling table by select query and adding table to dataset wich is going to be ItemSource for FilmsGrid

--- FilmsApp/MainWindow.xaml.cs
+++ FilmsApp/MainWindow.xaml.cs
@@ -1,5 +1,7 @@
-﻿using System;
+﻿using Npgsql;
+using System;
 using System.Collections.Generic;
+using System.Data;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -12,6 +14,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.Collections;
 
 namespace FilmsApp
 {
@@ -20,9 +23,37 @@ namespace FilmsApp
     /// </summary>
     public partial class MainWindow : Window
     {
+        private string strCon = @"Server=localhost;Port=5432;Username=postgres;Password=admin;Database=films";
+        private NpgsqlDataAdapter adapter = new NpgsqlDataAdapter();
+        public DataSet dataset = new DataSet();
         public MainWindow()
         {
             InitializeComponent();
+            var filmsTable = CreateDataTable();
+            FillTable(filmsTable);
+        }
+        private DataTable CreateDataTable()
+        {
+            DataTable table = new DataTable("Films");
+            table.Columns.Add("Id", typeof(int));
+            table.Columns.Add("name", typeof(string));
+            table.Columns.Add("rate", typeof(double));
+            table.Columns.Add("fdate", typeof(DateTime));
+            return table;
+        }
+        private void FillTable(DataTable table)
+        {
+            using (NpgsqlConnection con = new NpgsqlConnection(strCon))
+            {
+                con.Open();
+                var cmd = new NpgsqlCommand("select * from film", con);
+                table.Load(cmd.ExecuteReader());
+                //adapter = new NpgsqlDataAdapter(cmd);
+                //adapter.Fill(dataset);
+                dataset.Tables.Add(table);
+                FilmsGrid.ItemsSource = dataset?.Tables["Films"].DefaultView;
+            }
+            
         }
     }
 }
