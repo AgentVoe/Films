2f762bd59aadafa050ac400831f78675fd91e630
Voe <mr.subiter@mail.ru>
2023-03-20 23:57:30 +0500

    Create class "Films" for future development

---
+++ FilmsApp/Films.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web;
+
+namespace FilmsApp
+{
+    public class Films
+    {
+        private readonly int id;
+        public int Id { get { return id; } }
+        private string fname;
+        public string Fname { get { return fname; } set { fname = value; } }
+        private double rate;
+        public double Rate { get { return rate; } set { rate = value; } }
+    }
+}
